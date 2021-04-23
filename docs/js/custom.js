c2c7793e5f635f2aa54678b3caadd081e9f32e7b
kalo.aron <kalo.aron@appsint.hu>
2021-04-23 12:28:18 +0200

    feat: docs

---
+++ docs/js/custom.js
@@ -0,0 +1,5 @@
+﻿
+export const changeColorScheme = (light) => {
+    document.body.style.transitionDuration = '3.0s';
+    document.body.style.backgroundColor = (light === true) ?  '#FFFFFF' : '#353C42';
+}
